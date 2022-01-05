2ae7799f1fe89215d0b33633b2929d1753bf67e6
Takuyay <mechanicalgiraffe@gmail.com>
2022-01-05 17:42:52 +0900

    add require dotenv on db setting

--- db/index.js
+++ db/index.js
@@ -1,12 +1,14 @@
 const { Pool } = require("pg");
 const { DB } = require("../config");
+require("dotenv").config();
 
 const devConfig = `postgresql://${DB.USERNAME}:${DB.PASSWORD}@${DB.HOST}:${DB.PORT}/${DB.DATABASE}`;
 
 const proConfig = process.env.DATABASE_URL;
 
 const pool = new Pool({
-  connectionString: proConfig,
+  connectionString:
+    process.env.NODE_ENV === "production" ? proConfig : devConfig,
 });
 
 module.exports = {
